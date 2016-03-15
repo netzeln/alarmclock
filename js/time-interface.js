84016f1dbf71a8b4cefed82a508958352ad6002a
erik tolentino and nic netzel <tolentino3575@gmail.com>
2016-03-15 14:27:36 -0700

    add working things

--- js/time-interface.js
+++ js/time-interface.js
@@ -3,16 +3,31 @@ var date = null;
 
 var update = function(){
   date = moment(new Date())
-  datetime.html(date.format("dddd, h:m:ss a"));
+  datetime.html(date.format("dddd, h:mm:ss a"));
 }
 
 $(document).ready (function(){
-  // $('#time').text(moment().format("h:m:s a"));
+  $('#time').text(moment().format("h:m:s a"));
+  // $('#time').text(moment().format("YYYY-MM-DDTHH:mm"));
   datetime = $('#time')
   update();
   setInterval(update, 1000);
 
-  var alarm_time = ($input#set_alarm).val();
-
+$("form#time_form").submit(function(event){
   event.preventDefault();
+  var alarm_time = $("input#set_alarm").val();
+
+  $('#alarm_set_for').append(alarm_time);
+
+  setInterval(function(){
+    var current_time = moment().format("YYYY-MM-DDTHH:mm");
+     if(alarm_time === current_time){
+       $(".buzzer").toggle();
+      //  var current_time = moment().format("YYYY-MM-DDTHH:mm");
+     }else{
+       $(".buzzer").hide();
+     }
+   }, 500);
+});
+
 });
