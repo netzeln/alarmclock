be40a304015605730208576034dc36a0377999ff
erik tolentino and nic netzel <tolentino3575@gmail.com>
2016-03-15 12:01:15 -0700

    break for lunch

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,6 +1,18 @@
+var datetime = null;
+var date = null;
+
+var update = function(){
+  date = moment(new Date())
+  datetime.html(date.format("dddd, h:m:ss a"));
+}
+
 $(document).ready (function(){
-  $('#time').text(moment());
-  
+  // $('#time').text(moment().format("h:m:s a"));
+  datetime = $('#time')
+  update();
+  setInterval(update, 1000);
+
+  var alarm_time = ($input#set_alarm).val();
 
   event.preventDefault();
 });
